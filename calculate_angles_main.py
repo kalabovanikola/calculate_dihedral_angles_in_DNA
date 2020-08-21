bb008e268076489a514314a9edc22236912c2d3d
kalabovanikola <nikola@kalabova.eu>
2020-08-21 13:51:11 +0200

    tau and NN CC distances added

--- calculate_angles_main.py
+++ calculate_angles_main.py
@@ -6,7 +6,9 @@ import gemmi
 from gemmi import cif
 import numpy as np
 import csv
+import locale
 from copy import *
+from locale import atof
 
 """
 must_have = ["O3'", "P", "O5'", "C5'", "C4'", "C3'", "C1'", "N9" or "N1", "O4'", "C2'"]
@@ -121,6 +123,27 @@ class Angles:
                 if seq2.base_type == "purine" \
                 else calculate_dihedral_angles(seq1, seq2, [], ["O4'", "C1'", "N1", "C2"])
             self.NCCN_tors = calculate_NCCN_torsion(seq1, seq2)
+            self.taus = Taus(seq1, seq2)
+            self.NNdist = get_NNdist(seq1, seq2)
+            self.CCdist = get_distance(seq1.atoms["C1'"].coords, seq2.atoms["C1'"].coords)
+
+
+class Taus:
+    def __init__(self, seq1, seq2):
+        self.tau0_1 = calculate_dihedral_angles(seq1, seq2, ["C4'", "O4'", "C1'", "C2'"], [])
+        self.tau1_1 = calculate_dihedral_angles(seq1, seq2, ["O4'", "C1'", "C2'", "C3'"], [])
+        self.tau2_1 = calculate_dihedral_angles(seq1, seq2, ["C1'", "C2'", "C3'", "C4'"], [])
+        self.tau3_1 = calculate_dihedral_angles(seq1, seq2, ["C2'", "C3'", "C4'", "O4'"], [])
+        self.tau4_1 = calculate_dihedral_angles(seq1, seq2, ["C3'", "C4'", "O4'", "C1'"], [])
+        self.tau0_2 = calculate_dihedral_angles(seq1, seq2, [], ["C4'", "O4'", "C1'", "C2'"])
+        self.tau1_2 = calculate_dihedral_angles(seq1, seq2, [], ["O4'", "C1'", "C2'", "C3'"])
+        self.tau2_2 = calculate_dihedral_angles(seq1, seq2, [], ["C1'", "C2'", "C3'", "C4'"])
+        self.tau3_2 = calculate_dihedral_angles(seq1, seq2, [], ["C2'", "C3'", "C4'", "O4'"])
+        self.tau4_2 = calculate_dihedral_angles(seq1, seq2, [], ["C3'", "C4'", "O4'", "C1'"])
+        self.tau1, self.t1_max = get_tau(self.tau0_1, self.tau1_1, self.tau2_1, self.tau3_1, self.tau4_1)
+        self.tau1_type = get_pseudorotation_name(self.tau1)
+        self.tau2, self.t2_max = get_tau(self.tau0_2, self.tau1_2, self.tau2_2, self.tau3_2, self.tau4_2)
+        self.tau2_type = get_pseudorotation_name(self.tau2)
 
 
 class Sequence:
@@ -162,6 +185,84 @@ class WrongNumberOfArgumentsException(Exception):
 
     def __str__(self):
         return f'{self.amount} -> {self.message}'
+
+
+def get_NNdist(seq1, seq2):
+    """
+    get the distance between two N on bases
+    :param seq1:
+    :param seq2:
+    :return: euclidean distance
+    """
+    if seq1.base_type == "purine" and seq2.base_type == "purine":
+        return get_distance(seq1.atoms["N9"].coords, seq2.atoms["N9"].coords)
+    if seq1.base_type == "pyrimidine" and seq2.base_type == "purine":
+        return get_distance(seq1.atoms["N1"].coords, seq2.atoms["N9"].coords)
+    if seq1.base_type == "purine" and seq2.base_type == "pyrimidine":
+        return get_distance(seq1.atoms["N9"].coords, seq2.atoms["N1"].coords)
+    if seq1.base_type == "pyrimidine" and seq2.base_type == "pyrimidine":
+        return get_distance(seq1.atoms["N1"].coords, seq2.atoms["N1"].coords)
+
+
+def get_distance(atom1: Coords, atom2: Coords):
+    """
+
+    :param atom1: coords of the first atom
+    :param atom2: coords of the second one
+    :return: euclidean distance between two atoms
+    """
+    a = np.array((float(atom1.x), float(atom1.y), float(atom1.z)))
+    b = np.array((float(atom2.x), float(atom2.y), float(atom2.z)))
+    return round(np.linalg.norm(a - b), 2)
+
+
+def get_tau(tau0, tau1, tau2, tau3, tau4):
+    tau2 = 0.1 if tau2 == 0 else tau2
+    tau0, tau1, tau2, tau3, tau4 = normalize_taus([tau0, tau1, tau2, tau3, tau4])
+    tan_p = ((tau4 + tau1) - (tau3 + tau0)) / (
+                2 * tau2 * (math.sin(36.0 * math.pi / 180.0) + math.sin(72.0 * math.pi / 180.0)))
+    p = math.atan(tan_p) * 180.0 / math.pi
+    if tau2 < 0:
+        p = p + 180.0
+    elif tan_p < 0:
+        p = p + 360.0
+    tmax = abs(tau2 / math.cos(p * math.pi / 180.0))
+    return "%.1f" % p, "%.1f" % tmax
+
+
+def normalize_taus(taus):
+    taus = [tau - 360.0 if tau > 180.0 else tau for tau in taus]
+    return taus
+
+
+def get_pseudorotation_name(P):
+    """
+    For the given pseudorotation angle return its name. Based on Altona, Sundaralingam, JACS 94:23, 1972, 8205-8212
+    :param P: pseudorotation
+    :return: type of pseudorotation
+    """
+    P = atof(P)
+    if ((P >= 0.0) and (P < 36.0)) or (P == 360.0):
+        return "C3end"
+    if (P >= 36.0) and (P < 72.0):
+        return "C4exo"
+    if (P >= 72.0) and (P < 108.0):
+        return "O4end"
+    if (P >= 108.0) and (P < 144.0):
+        return "C1exo"
+    if (P >= 144.0) and (P < 180.0):
+        return "C2end"
+    if (P >= 180.0) and (P < 216.0):
+        return "C3exo"
+    if (P >= 216.0) and (P < 252.0):
+        return "C4end"
+    if (P >= 252.0) and (P < 288.0):
+        return "O4exo"
+    if (P >= 288.0) and (P < 324.0):
+        return "C1end"
+    if (P >= 324.0) and (P < 360.0):
+        return "C2exo"
+    return ""
 
 
 def get_step_name(seq1: SequenceVariant, seq2: SequenceVariant, model_number, is_the_only_one):
@@ -272,8 +373,6 @@ def parse_to_entities(table):
         if row[10] != k:  # models
             array_of_models.append(Model(row[10]))
             k = row[10]
-            array_of_models[-1].entities.append(Entity())
-            n = row[1]
         if row[1] != n or (row[2] != m and row[2] == "1"):  # entities
             array_of_models[-1].entities.append(Entity())
             n = row[1]
@@ -397,7 +496,9 @@ def get_cif_name():
 def write_to_csv():
     with open(get_cif_name() + '.csv', 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["step_ID", "d1", "e1", "z1", "a2", "b2", "g2", "d2", "ch1", "ch2", "mu"])
+        writer.writerow(["step_ID", "d1", "e1", "z1", "a2", "b2", "g2", "d2", "ch1", "ch2", "NN", "CC", "mu", "P1",
+                         "t1", "Pn1", "P2", "t2", "Pn2", "nu11", "nu12", "nu13", "nu14", "nu15", "nu21", "nu22", "nu23",
+                         "nu24", "nu25"])
         for _model in _models_array:
             for _entity in _model.entities:
                 for _atom in _entity.steps:
@@ -405,7 +506,13 @@ def write_to_csv():
                         [_atom.name, _atom.angles.delta, _atom.angles.epsilon, _atom.angles.zeta, _atom.angles.alpha,
                          _atom.angles.beta,
                          _atom.angles.gamma, _atom.angles.delta2, _atom.angles.chi, _atom.angles.chi2,
-                         _atom.angles.NCCN_tors])
+                         _atom.angles.NNdist, _atom.angles.CCdist, _atom.angles.NCCN_tors, _atom.angles.taus.tau1,
+                         _atom.angles.taus.t1_max, _atom.angles.taus.tau1_type, _atom.angles.taus.tau2,
+                         _atom.angles.taus.t2_max, _atom.angles.taus.tau2_type, _atom.angles.taus.tau0_1,
+                         _atom.angles.taus.tau1_1, _atom.angles.taus.tau2_1, _atom.angles.taus.tau3_1,
+                         _atom.angles.taus.tau4_1, _atom.angles.taus.tau0_2,
+                         _atom.angles.taus.tau1_2, _atom.angles.taus.tau2_2, _atom.angles.taus.tau3_2,
+                         _atom.angles.taus.tau4_2])
 
 
 _models_array = split_into_entities_and_calculate_parameters()
